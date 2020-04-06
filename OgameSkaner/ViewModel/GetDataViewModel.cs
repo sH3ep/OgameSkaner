17d645d28a561ede0e3ed2a07c5ffb1c13581258
Karol Pytka <karol.pytka@coffeemug.io>
2020-04-06 17:02:21 +0200

    dsdsdsd

--- OgameSkaner/ViewModel/GetDataViewModel.cs
+++ OgameSkaner/ViewModel/GetDataViewModel.cs
@@ -20,7 +20,7 @@ namespace OgameSkaner.ViewModel
     {
         public GetDataViewModel(IGameRestClient gameRestClient)
         {
-            _gameRestClient = gameRestClient;//cxcxcxcxzxcvxzcvzxdsdsd
+            _gameRestClient = gameRestClient;//cxcxcxcxzxcvxzcvzxdsdsddsdsdsd
             usersPlanets = new ObservableCollection<UserPlanet>();
             GetSolarSystemsDataCommand = new DelegateCommand(async () => { await GetSolarSystemAsync(); }, CanExecute);
             LogInCommand = new DelegateCommand(async () => { await LogIn(); }, CanExecute);
